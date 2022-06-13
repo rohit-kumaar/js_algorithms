90adbf340520c9b0c2503f72484afe5e5ca1470d
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-06-13 12:09:30 +0530

    remove duplicate  value in array of object

--- d_remove_duplicate_value.js
+++ d_remove_duplicate_value.js
@@ -1,7 +1,17 @@
-// Method - I
+// Method - I Array
 // const removeDuplicateValue = (arrays) =>
 //   arrays.filter((element, index, array) => array.indexOf(element) === index);
 
-// Method - II
-const removeDuplicateValue = (params) => [...new Set(params)];
-console.log(removeDuplicateValue(numbers));
+// Method - II Array
+// const removeDuplicateValue = (params) => [...new Set(params)];
+// console.log(removeDuplicateValue(numbers));
+
+// Method - III Array of object
+const companies = [
+  { name: "Company One" },
+  { name: "Company Two" },
+  { name: "Company Two" },
+];
+
+const removeDuplicateValue = (params) => new Set(params);
+console.log(removeDuplicateValue(companies.map(JSON.stringify)));
