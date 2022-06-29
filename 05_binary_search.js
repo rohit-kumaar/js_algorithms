b5654b181c2b18efc9cdd95d596d4dc354004333
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-06-29 12:21:01 +0530

    binary search

--- 05_binary_search.js
+++ 05_binary_search.js
@@ -3,3 +3,30 @@
 /* Creating an array of numbers. */
 const array = [2, 4, 10, 30, 28, 35, 41, 96, 2];
 
+/* Sorting the array in ascending order. */
+array.sort((a, b) => a - b);
+
+const binarySearch = (array, target) => {
+  let leftIndex = 0;
+  let rightIndex = array.length - 1;
+
+  while (leftIndex <= rightIndex) {
+    const middleIndex = Math.floor((leftIndex + rightIndex) / 2);
+
+    if (target === array[middleIndex]) return middleIndex;
+
+    // target < array[middleIndex]
+    //   ? (rightIndex = middleIndex - 1)
+    //   : (leftIndex = middleIndex + 1);
+
+    if (target < array[middleIndex]) {
+      rightIndex = middleIndex - 1;
+    } else {
+      leftIndex = middleIndex + 1;
+    }
+  }
+
+  return -1;
+};
+
+console.log(binarySearch(array, 30));
