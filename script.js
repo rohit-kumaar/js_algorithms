0ca6e69cf487b9280113ca8d8884752f0adac110
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-07-01 11:09:56 +0530

    merge sort

--- script.js
+++ script.js
@@ -15,7 +15,8 @@ function script(url) {
 // script("./search/03_recursive_binary_search.js");
 // script("./sort/01_bubble_sort.js");
 // script("./sort/02_insertion_sort.js");
-script("./sort/03_quick_sort.js");
+// script("./sort/03_quick_sort.js");
+script("./sort/04_merge_sort.js");
 // script("./a_fibonacci.js");
 // script("./b_gridTraveler.js");
 // script("./c_anagram.js");
